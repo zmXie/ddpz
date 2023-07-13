e1aee25754ad5887447d31ced4eeadf9422d72cb
xiezhimin <xiezm@dachentech.com.cn>
2023-07-13 18:09:57 +0800

    feat: modify

--- components/deploy.sh
+++ components/deploy.sh
@@ -1,5 +1,13 @@
 # node update_version.js && npm publish && cd .. && npm run push
-node update_version.js
-npm publish
-cd ..
+
+if node update_version.js; then
+    if npm publish; then
+        echo '发布成功'
+        cd ..
 npm run push
+    fi
+
+fi
+
+
+
